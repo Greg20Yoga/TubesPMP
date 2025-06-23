813556117fd2a9f2788b6f29d14377f315e919c3
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-23 20:10:27 +0700

    Create tampilkanJumlahPelanggaran.h

---
+++ Coding/Menu Utama/tampilkanJumlahPelanggaran.h
@@ -0,0 +1,8 @@
+#ifndef TAMPILKANJUMLAHPELANGGARAN_H
+#define TAMPILKANJUMLAHPELANGGARAN_H
+
+extern int pelanggaran_preferensi;  // Deklarasi extern di header
+
+void tampilkanJumlahPelanggaran();
+
+#endif
