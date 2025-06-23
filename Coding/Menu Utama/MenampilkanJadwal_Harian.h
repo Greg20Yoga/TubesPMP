3c5d59a0fc2ff5b520d8d81e50a5d1175c0bd99a
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-23 19:51:50 +0700

    Create MenampilkanJadwal_Harian.h

---
+++ Coding/Menu Utama/MenampilkanJadwal_Harian.h
@@ -0,0 +1,10 @@
+#ifndef MENAMPILKANJADWAL_HARIAN_H
+#define MENAMPILKANJADWAL_HARIAN_H
+
+#include "dokter.h"  
+#include "jadwal.h"  
+
+// Deklarasi fungsi untuk menampilkan jadwal harian
+void tampilkanJadwal_Harian(hari* head);
+
+#endif
