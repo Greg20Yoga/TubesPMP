fe926c53a146ddfb47458db85ed8558ada4a090c
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-23 20:08:01 +0700

    Create tampilkanJumlahPelanggaran.c

---
+++ Coding/Menu Utama/tampilkanJumlahPelanggaran.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tampilkanJumlahPelanggaran.h"
+
+void tampilkanJumlahPelanggaran() {
+    printf("\n🚨 Jumlah Pelanggaran: %d\n", jumlahPelanggaran);
+}
