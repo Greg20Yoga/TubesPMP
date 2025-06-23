58fe7bc5695ef7f7078f3af386622c05680f1a15
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-23 14:44:01 +0700

    Create menu_olah_data.h

---
+++ Coding/Menu Utama/menu_olah_data.h
@@ -0,0 +1,12 @@
+#ifndef MENUOLAHDATA_H
+#define MENUOLAHDATA_H
+
+// Deklarasi fungsi menu olah data
+void menu_olah_data();
+
+// Deklarasi fungsi-fungsi operasi dokter
+void tambahDokter();
+void hapusDokter();
+void tampilkanData();
+
+#endif
