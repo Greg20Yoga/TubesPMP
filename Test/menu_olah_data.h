cf0a8bf6010b4f773eae6e2a411a8d40aea3df6d
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 10:40:48 +0700

    Update menu_olah_data.h

--- Test/menu_olah_data.h
+++ Test/menu_olah_data.h
@@ -1,1 +1,15 @@
+#ifndef MENUOLAHDATA_H
+#define MENUOLAHDATA_H
 
+#include "Dokter.h" 
+#include "Jadwal.h"
+
+// Deklarasi fungsi menu olah data
+void menu_olah_data();
+
+// Deklarasi fungsi-fungsi operasi dokter
+void tambahDokter();
+void hapusDokter();
+void tampilkanData();
+
+#endif
